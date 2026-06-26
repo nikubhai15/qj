6439c0e81e0b3d3ac9418fd860f0dfb9099f6cb2
nikubhai15 <nikubhai155@gmail.com>
2026-06-26 12:18:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-greenlet>=3.1.0
-playwright==1.46.0
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+pip install -r requirements.txt --ignore-dependencies
+pip install playwright==1.46.0 greenlet==3.0.3
+playwright install chromium
